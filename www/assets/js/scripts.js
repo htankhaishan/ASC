23660241333a68f9ab301cf170daf246533f864c
Kyaw Htet Thu <htankhaishan@gmail.com>
2025-03-02 21:15:50 +0800

    Delete www directory

--- www/assets/js/scripts.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.querySelectorAll("button");
-
-    buttons.forEach(button => {
-        button.addEventListener("click", function () {
-            alert("Button clicked! (Insecure JavaScript Example)");
-        });
-    });
-});
